5dab0dbc8e896df83a27ab03097f8ded22418e84
alexterua <alexter.od.ua@gmail.com>
2018-07-11 00:55:24 +0300

    Добавлены эффекты наведения у кнопок и продуктов, реализован дропдаун, переразмечены продукты

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 "use strict";
+window.onload = function () {
+    var dropdown = document.querySelector('.dropdown');
+    var buttonDropdown = document.querySelector('.btn-dropdown');
+
+    buttonDropdown.addEventListener('click', function (e) {
+        e.preventDefault();
+        if (dropdown.classList.contains('visually-hidden')) {
+            dropdown.classList.remove('visually-hidden');
+        } else {
+            dropdown.classList.add('visually-hidden');
+        }    
+    });
+
+    window.addEventListener('keydown', function (e) {
+        e.preventDefault();
+        if (!dropdown.classList.contains('visually-hidden') || (e.keyCode === '27')) {
+            dropdown.classList.add('visually-hidden');
+        }
+    });
+};
+
